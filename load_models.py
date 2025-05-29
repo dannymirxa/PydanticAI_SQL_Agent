3193bb59da472e8badcc334ab77dfbf03e4066cb
accenturedanialmirza <danial.m.bin.madrawi@accenture.com>
2025-05-29 17:44:58 +0800

    add vectordb using pgvector

--- load_models.py
+++ load_models.py
@@ -1,5 +1,4 @@
-from openai import AsyncAzureOpenAI
-
+from openai import AsyncAzureOpenAI, AzureOpenAI
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
@@ -15,19 +14,27 @@ AZURE_OPENAI_KEY = os.getenv("AZURE_OPENAI_KEY")
 
 # GEMINI_MODEL = GeminiModel('gemini-2.0-flash', provider=GoogleGLAProvider(api_key=GEMINI_API_KEY))
 
-client = AsyncAzureOpenAI(
+async_client = AsyncAzureOpenAI(
     azure_endpoint = "https://llmcoechangemateopenai2.openai.azure.com/",
     api_key=AZURE_OPENAI_KEY,
-    api_version="2024-10-01-preview",
+    api_version="2024-10-21",
+    azure_deployment='gpt-4o'
 )
 
 OPENAI_MODEL = OpenAIModel(
     'gpt-4o-dev',
-    provider=OpenAIProvider(openai_client=client),
+    provider=OpenAIProvider(openai_client=async_client),
 )
 
-# from pydantic_ai import Agent
-# agent = Agent(model)
-# result = agent.run_sync('Where is KL, Malaysia?')
+client = AzureOpenAI(
+    azure_endpoint = "https://llmcoechangemateopenai2.openai.azure.com/",
+    api_key=AZURE_OPENAI_KEY,
+    api_version="2024-10-21",
+)
 
-# print(result)
+def get_embedding(text, model="text-embedding-ada-002"): # model = "deployment_name"
+    return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+# print(generate_embeddings("What is the meaning of life?"))
+
+
